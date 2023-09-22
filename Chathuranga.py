09af366c27f6744e0003c77f8c74d5d92649f56c
Chathur0 <145315287+Chathur0@users.noreply.github.com>
2023-09-22 17:27:14 +0530

    Chathuranga commit

--- Chathuranga.py
+++ Chathuranga.py
@@ -2,3 +2,7 @@ numList = [x for x in range(10)]
 print(numList)
 for x in newlist:
     print(isinstance(x,int))
+numList = [x for x in range(10)]
+print(numList)
+for x in newlist:
+    print(isinstance(x,int))
